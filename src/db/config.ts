aaae9edbc18dcc0835ebcf9193509079a2987d37
jswithh <jswithhari@gmail.com>
2024-09-04 16:38:01 +0700

    update package and config

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,14 +3,14 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  schema: "./src/domain/**/**.schema.ts",
+  schema: "./src/domain/**/schema.ts",
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST ?? "",
-    port: parseInt(process.env.DB_PORT ?? "3306"),
-    database: process.env.DB_NAME ?? "",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST ?? "",
+    port: parseInt(process.env.MYSQL_PORT ?? "3306"),
+    database: process.env.MYSQL_DATABASE ?? "",
   },
 } satisfies Config;
